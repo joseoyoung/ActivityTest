5b3f8c3f812ea186f5ff5fe485b598837205f874
joseoyoung <josy3465@naver.com>
2017-04-13 20:13:59 +0900

    #1 Make activity

--- app/src/main/java/kr/hs/emirim/joseoyoung/activitytest/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/joseoyoung/activitytest/SecondActivity.java
@@ -2,6 +2,8 @@ package kr.hs.emirim.joseoyoung.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button butBack = (Button)findViewById(R.id.but_second);
+        butBack.setOnClickListener(butBackHandler);
     }
+    View.OnClickListener butBackHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();//사용했던 모든 객체들과 함께 secondactivity도 삭제됨.
+        }
+    };
 }
